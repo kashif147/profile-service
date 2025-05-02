2f37e735ccb84d2edd1833bd9acc33cdab13630f
sadeeq <sadeequk8@gmail.com>
2025-05-03 00:36:33 +0500

    📌 added templete

--- models/user.js
+++ models/user.js
@@ -28,3 +28,5 @@ const UserSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", UserSchema);
+// module.exports = mongoose.model("User", UserSchema);
+// module.exports = mongoose.model("User", UserSchema);
