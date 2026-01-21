dbc30c44364343713e21ba0b646b0a43ce78a770
dev-sadeeq0023 <sadeeqln8@gmail.com>
2026-01-21 20:45:11 +0500

    fixes

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -785,6 +785,11 @@ async function checkEmailExists(req, res, next) {
   }
 }
 
+/**
+ * Get personal details for the logged-in PORTAL user
+ * GET /api/profile/my-personal-details
+ * Returns: Personal details for the user
+ */
 async function getMyPersonalDetails(req, res, next) {
   try {
     const { userId, userType } = extractUserAndCreatorContext(req);
