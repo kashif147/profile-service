b7cae169bf8a546446ad79131553c8ae52a5b1a8
dev-sadeeq0023 <sadeeqln8@gmail.com>
2026-02-07 21:09:12 +0500

    fixes

--- config/azure.storage.js
+++ config/azure.storage.js
@@ -12,9 +12,9 @@ const {
 
 
 const connectionString = (process.env.AZURE_STORAGE_CONNECTION_STRING || "").replace(/\s+/g, " ").trim();
-const accountName = (process.env.AZURE_STORAGE_ACCOUNT || "").trim();
-const accountKey = (process.env.AZURE_STORAGE_KEY || "").replace(/\r?\n/g, "").trim();
-const containerName = (process.env.AZURE_STORAGE_CONTAINER || "").trim();
+const accountName = (process.env.AZURE_STORAGE_ACCOUNT).trim();
+const accountKey = (process.env.AZURE_STORAGE_KEY).replace(/\r?\n/g, "").trim();
+const containerName = (process.env.AZURE_STORAGE_CONTAINER).trim();
 
 let blobServiceClient = null;
 let sharedKeyCredential = null;
