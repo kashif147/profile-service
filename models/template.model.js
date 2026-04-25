972891e309dacf6e2d4ed4c838a43de53aa28222
Kashif <kashif147@gmail.com>
2026-04-25 15:53:33 +0100

    template update error 500 fix

--- models/template.model.js
+++ models/template.model.js
@@ -37,8 +37,7 @@ const TemplateSchema = new mongoose.Schema(
       default: [],
     },
     columnLabels: {
-      type: Map,
-      of: String,
+      type: mongoose.Schema.Types.Mixed,
       default: {},
     },
     isDefault: {
